5dfd272575017fc9a7be0d950e72ac61ad5a1ad3
terrellbm <tbmarshall97@gmail.com>
2025-04-01 15:13:39 -0700

    Rename spongecake folder to spongecake-sdk

--- spongecake/spongecake/__init__.py
+++
@@ -1,5 +0,0 @@
-from .desktop import Desktop
-from .agent import Agent
-from .constants import AgentStatus
-
-__all__ = ["Desktop", "AgentStatus", "Agent"]
--- spongecake/spongecake/_exceptions.py
+++
@@ -1,2 +0,0 @@
-class SpongecakeException(Exception):
-    pass
